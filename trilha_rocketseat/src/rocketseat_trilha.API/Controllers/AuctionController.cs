73e84ddc1b5ad7399482b9cb3c11da9e09db2de8
vitoriaguidines <vitoriaguidine@id.uff.br>
2024-02-06 16:35:23 -0300

    primeira aula

---
+++ trilha_rocketseat/src/rocketseat_trilha.API/Controllers/AuctionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+namespace RocketseatAuction.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuctionController : Controller
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult GetCurrentAuction()
+    {
+        var useCase = new GetCurrentAuctionUseCase();
+
+        var result = useCase.Execute();
+
+        if (result is null)
+            return NoContent();
+
+        return Ok(result);
+    }
+}
---
+++ trilha_rocketseat/src/rocketseat_trilha.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction? Execute()
+    {
+        var repository = new RocketseatAuctionDbContext();
+
+        return repository
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault();
+
+    }
+}
